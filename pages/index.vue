f72ffe518bd6ecd07121a0507c22f32be35f87c1
atooomik <50058130+atooomik@users.noreply.github.com>
2023-03-24 13:55:38 -0600

    clean querySearch

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,30 @@
 <template>
   <div class="bg-gray-900 w-full h-screen overflow-scroll">
-    <div
-      class="max-w-screen-2xl w-full mx-auto text-center border-2 border-gray-800 rounded-lg"
-    >
+    <div class="max-w-screen-2xl w-full mx-auto text-center">
       <div class="bg-gray-800 my-4 py-4 text-gray-500 rounded-lg">
         <p>Memeteca</p>
       </div>
-      <div class="bg-gray-800 text-gray-500">
-        <p>Busca por nombre</p>
+      <div class="bg-gray-800 text-gray-500 p-4 text-left">
+        <p class="py-2 text-gray-500">Busca por nombre</p>
         <input
           type="text"
           v-model="querySearch"
           aria-label="Busqueda por nombre o termino"
-          class="bg-gray-700 border border-gray-700 rounded-lg"
+          class="bg-gray-700 border border-gray-700 rounded-lg p-2"
         />
 
-        <button @click="filterMemes">Buscar</button>
-        <button @click="cleanSearch">Restablecer</button>
+        <button
+          @click="filterMemes"
+          class="bg-gray-700 rounded-lg px-2 py-2 ml-4 text-gray-500"
+        >
+          Buscar
+        </button>
+        <button
+          @click="cleanSearch"
+          class="bg-gray-700 rounded-lg px-2 py-2 text-gray-500 ml-4"
+        >
+          Restablecer
+        </button>
       </div>
       <div class="grid grid-cols-3 grid-rows-6 p-2 gap-6 pt-6">
         <sample-card
@@ -64,6 +72,7 @@ export default {
     },
     cleanSearch() {
       this.selectedData = this.originalData
+      this.querySearch = ''
     },
   },
 }
